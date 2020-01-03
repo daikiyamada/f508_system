552ab250d02b537407af32ad48370ff37699db08
yamadadaiki <daiki.baseball0516@gmail.com>
2020-01-03 21:04:51 +0900

    0103commit

--- setuser.php
+++ setuser.php
@@ -15,7 +15,22 @@
 <li id="menu3"><a href="calendar.html">予約状況確認</a></li>
 </ul>
 <h1 id =news_head>お知らせ</h1>
-<?php require_once 'Manager.php' ?>
+<?php require_once 'Manager.php';
+try{
+  //データベースに接続してPDOオブジェクトを作成
+  $db=connect();
+  $sql = 'INSERT INTO f508system(ID,Name,pw) VALUES(:ID,:Name,:pw)';
+  //プリペアドステートメントを生成
+  $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+  //プリペアドステートメントを実行
+  $stt->execute(array(':ID'=>$_POST['ID'],':Name'=>$_POST['Name'],':pw'=> $_POST['pw']));
+  $db = NULL;
+}catch (PDOException $e){
+  exit("エラーが発生しました:{$e->getMessage()}");
+}
+//処理完了後、登録ページを再表示
+header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/setuser.php')
+?>
 <script type="text/javascript" style="text-align: right;">
 <!--
   var modified = new Date(document.lastModified);
