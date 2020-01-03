957d17f9589ec0022942ff44bdc0eb793b0e4b62
yamadadaiki <daiki.baseball0516@gmail.com>
2020-01-03 21:41:58 +0900

    0103commit

--- Manager.php
+++ Manager.php
@@ -1,8 +1,8 @@
 <?php
 function connect(){
-  $dsn = 'mysql:dbname=student; host=localhost; charset=utf8';
+  $dsn = 'mysql:dbname=student; host=172.31.33.161; charset=utf8';
   $usr = 'root';
-  $passwd = "Daiki_06890516";
+  $passwd = 'Daiki_06890516';
   try{
     $db = new PDO($dsn, $usr,$passwd);
   }catch(PDOException $e){
