f2f809285c2646c30b13113d549fcb4f11e19c5c
yamadadaiki <daiki.baseball0516@gmail.com>
2020-01-04 14:38:05 +0900

    0104commit

--- Manager.php
+++ Manager.php
@@ -1,10 +1,10 @@
 <?php
 function connect(){
-  $dsn = 'mysql:dbname=student;host=localhost';
-  $usr = 'root';
+  $dsn = "mysql:dbname=student;host=localhost";
+  $usr = "root";
   $pass = "daiki06890516";
   try{
-    $db = new PDO($dsn, $usr, $pass);
+    $db = new PDO($dsn, $usr,$pass);
   }catch(PDOException $e){
     exit("データベース接続失敗:{$e->getMessage()}");
   }
