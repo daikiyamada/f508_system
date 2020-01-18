e402e848b39304274e22d2e8d3db2b4d9bc1156a
yamadadaiki <daiki.baseball0516@gmail.com>
2020-01-18 14:58:28 +0900

    0118commit

--- Refresh.php
+++ Refresh.php
@@ -1,12 +1,14 @@
 <?php require_once 'Manager.php';
 try{
   $db = connect();
-  $sql = 'DELETE FROM f508system
-         WHERE ID=:ID';
-  print "$_POST['ID']";
+  $sql = 'DELETE FROM f508system WHERE ID=:ID'; //SQL文の作成
+  $stt = $db->prepare($sql));
+  for($i=1;$i<=$_POST['ct'];$i++){
+    $stt -> execute(array(':ID' =>$_POST['ID'.$i]));
+  }
 }catch(PDOException $e){
   die("エラーが発生: {$e ->getMEssage()}");
 }
 $db = NULL;
-//header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/userlists.php');
+header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/userlists.php');
  ?>
