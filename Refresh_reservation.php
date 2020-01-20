c59ae2596909d43bbb12d51a39fc42e770ac3f17
1558121 <e19m5210@soka-u.jp>
2020-01-20 21:22:04 +0900

    Add del_reservation.php del.php Refresh_reservation.php

---
+++ Refresh_reservation.php
@@ -0,0 +1,13 @@
+<?php require_once 'Manager.php';
+try{
+  $db = connect();
+  $sql = "DELETE FROM Reservation WHERE =:ReservationNumber"; //SQL文の作成
+  $stt = $db->prepare($sql);
+  $stt -> execute(array(':ReservationNumber' =>$_POST['ReservationNumber']));
+  print $_POST['ReservationNumber'];
+}catch(PDOException $e){
+  die("エラーが発生: {$e ->getMEssage()}");
+}
+$db = NULL;
+//header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/userlists.php');
+ ?>
---
+++ del.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<?xml version="1.0" encoding="utf-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml="" lang="ja" lalng="ja" xml:lang="ja">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+  <title>研究室管理システム</title>
+</head>
+  <link rel="stylesheet" type="text/css" href="Homepage.css" />
+<body>
+  <div id="back1">
+    <hr id="line1"/>
+    <h1 id="title1">予約削除</h1>
+  </div>
+<ul id="menu">
+<li><a href="index.html">Home</a></li>
+<li><a href="calendar.html">F508管理システム</a></li>
+</ul>
+<h1 id =news_head>お知らせ</h1>
+<?php
+require_once 'Manager.php';
+try{
+  //データベースに接続してPDOオブジェクトを作成
+  $db=connect();
+  $sql = 'DELETE FROM Reservation WHERE ReservationNumber=:ReservationNumber';
+  //プリペアドステートメントを生成
+  $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+  //プリペアドステートメントを実行
+  $stt->execute(array(':ReservationNumber'=>$_POST['ReservationNumber']));
+  $db = NULL;
+}catch (PDOException $e){
+  exit("エラーが発生しました:{$e->getMessage()}");
+}
+//処理完了後、登録ページを再表示
+header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/del_reservation.php')
+?>
+<script type="text/javascript" style="text-align: right;">
+<!--
+  var modified = new Date(document.lastModified);
+  var yy = modified.getFullYear();
+  var mm= modified.getMonth() + 1;
+  var dd = modified.getDate();
+  document.write('最終更新日:' + yy + '年' + mm + '月' + dd + '日');
+  //
+-->
+
+</body>
+</html>
