c320f267092425fe2925e7eaae05532cd3798bd5
yamadadaiki <daiki.baseball0516@gmail.com>
2020-01-27 18:20:09 +0900

    0127commit

--- Reservation/form.php
+++ Reservation/form.php
@@ -21,11 +21,11 @@ require_once 'Manager.php';
   try{
     //データベースに接続してPDOオブジェクトを作成
     $db=connect();
-    $sql = 'INSERT INTO Reservation(date,class,ID,purpose) VALUES(:date,:class,:ID,:purpose)';
+    $sql = 'INSERT INTO Reservation(reserveID,date,class,ID,purpose) VALUES(:reserveID,:date,:class,:ID,:purpose)';
     //プリペアドステートメントを生成
     $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
     //プリペアドステートメントを実行
-    $stt->execute(array(':date'=>$_POST['date'],'class'=>$_POST['class'],':ID'=>$_POST['ID'],':purpose'=> $_POST['purpose']));
+    $stt->execute(array(':reserveID'=>$_POST['reserveID'],':date'=>$_POST['date'],'class'=>$_POST['class'],':ID'=>$_POST['ID'],':purpose'=> $_POST['purpose']));
     $db = NULL;
   }catch (PDOException $e){
     exit("エラーが発生しました:{$e->getMessage()}");
