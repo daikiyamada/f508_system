6eaaf02b20cb4cc3c0d221b0ac8bec26ab4731fb
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-02 21:24:11 +0900

    0202commit

--- Reservation/del.php
+++ Reservation/del.php
@@ -21,17 +21,20 @@ require_once 'Manager.php';
 try{
   //データベースに接続してPDOオブジェクトを作成
   $db=connect();
-  $sql = 'DELETE FROM Reservation WHERE date = :date and class = :class and ID = :ID';
+  $sql = 'DELETE FROM Reservation WHERE date=:date and class = :class and ID = :ID';
   //プリペアドステートメントを生成
   $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
   //プリペアドステートメントを実行
+  print $_POST['date']
+  print $_POST['class']
+  print $_POST['ID']
   $stt->execute(array(':date' => $_POST['date'],':class' => $_POST['class'], ':ID' => $_POST['ID']));
   $db = NULL;
 }catch (PDOException $e){
   exit("エラーが発生しました:{$e->getMessage()}");
 }
 //処理完了後、登録ページを再表示
-header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/calendar.html')
+//header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/calendar.html')
 ?>
 <script type="text/javascript" style="text-align: right;">
 <!--
