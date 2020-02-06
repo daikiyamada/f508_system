86fcf714d1f52748f3048dd3b6f78fb162c6f384
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-06 17:23:36 +0900

    0206commit

--- Reservation/auto_del.php
+++ Reservation/auto_del.php
@@ -3,14 +3,14 @@ require_once 'Manager.php';
 try{
   //データベースに接続してPDOオブジェクトを作成
   $db=connect();
-  $now = date('Ymd7');
+  $now = date('Ymd');
   $sql = 'DELETE FROM Reservation WHERE date = :date and class = :class';
   //プリペアドステートメントを生成
   $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
   //プリペアドステートメントを実行
-  for(int $i=0;$i<7;$i++){
+  /*for(int $i=0;$i<7;$i++){
     $stt->execute(':date' => $now, ':class' => $i);
-}
+}*/
   $db = NULL;
 }catch (PDOException $e){
   exit("エラーが発生しました:{$e->getMessage()}");
@@ -18,5 +18,6 @@ try{
 //処理完了後、登録ページを再表示
 //header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/calendar.html')
 ?>
+aaaa
 </body>
 </html>
