f766e96a6fdab4954f0b04742d53d3794e648d38
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-06 17:14:14 +0900

    0206commit

--- Reservation/auto_del.php
+++ Reservation/auto_del.php
@@ -5,11 +5,11 @@ try{
   $db=connect();
   $now = date('Ymd');
   print $now;
-  $sql = 'DELETE FROM Reservation WHERE date < $now';
+  $sql = 'DELETE FROM Reservation WHERE date =:date';
   //プリペアドステートメントを生成
   $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
   //プリペアドステートメントを実行
-  $stt->execute();
+  $stt->execute(':date' => $now);;
   $db = NULL;
 }catch (PDOException $e){
   exit("エラーが発生しました:{$e->getMessage()}");
