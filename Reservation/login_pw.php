5dc323b113770ee7035ee0caf3901d2a2048d34b
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-09 13:37:18 +0900

    0209commit

--- Reservation/login_pw.php
+++ Reservation/login_pw.php
@@ -11,9 +11,9 @@
       if($alert)header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/login.php');
     }
     //ログイン認証の成功
-    session_start();
-    session_regenerate_id(true); //セッションIDを振り直す
-    $_SESSION['ID'] = $_POST['ID'];
+    //session_start();
+    //session_regenerate_id(true); //セッションIDを振り直す
+    //$_SESSION['ID'] = $_POST['ID'];
     ?>
     <script type='text/javascript'>
     window.alert('ログインしました');
