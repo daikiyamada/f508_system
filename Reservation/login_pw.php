082f8daffe4066fbffd7c11be148ddfc1b99615c
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-09 13:23:25 +0900

    0209commit

--- Reservation/login_pw.php
+++ Reservation/login_pw.php
@@ -14,7 +14,8 @@
     session_regenerate_id(true); //セッションIDを振り直す
     $_SESSION['ID'] = $_POST['ID'];
     $alert = "<script type='text/javascript'> alert('ログインしました')</script>";
-    header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'calendar.html');
+    print $alert;
+    header('Location: http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/calendar.html');
     $db = NULL;
   }catch (PDOException $e){
     exit("エラーが発生しました:{$e->getMessage()}");
