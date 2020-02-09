ccb4c67f906233025f050e6d819988ce831cd7ac
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-09 12:34:23 +0900

    0209commit

--- Reservation/form.php
+++ Reservation/form.php
@@ -21,6 +21,7 @@ require_once 'Manager.php';
   try{
     //データベースに接続してPDOオブジェクトを作成
     $db=connect();
+
     $sql = 'INSERT INTO Reservation(reserveID,date,class,ID,purpose) VALUES(:reserveID,:date,:class,:ID,:purpose)';
     //プリペアドステートメントを生成
     $stt = $db ->prepare($sql,array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
