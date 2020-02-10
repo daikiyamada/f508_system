2950de01c8ca0f9f9e78b167365bccda2db377aa
1558121 <e19m5210@soka-u.jp>
2020-02-10 15:02:36 +0900

    Fix calendar.js

--- Reservation/json.php
+++ Reservation/json.php
@@ -7,8 +7,10 @@ mb_http_input("auto");
 mb_http_output("utf-8");
 
 $dbh = connect();
-
-$sth = $dbh->prepare("SELECT * FROM Reservation");
+String date1 = year + month + "01" + "0"
+String date2 = year + month + "31" + "8"
+$sth = $dbh->prepare("SELECT * FROM Reservation where Date between :date1 and : date2");
+// 月日を文字列で取得して，数字に変更して配列に入れる
 $sth->execute();
 
 $userData = array();
@@ -24,4 +26,5 @@ while($row = $sth->fetch(PDO::FETCH_ASSOC)){
 
 //jsonとして出力
 header('Content-type: application/json');
-echo json_encode($userData);
+echo json_encode($userData, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+file_put_contents("my.json", json_encode($userData, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT));
