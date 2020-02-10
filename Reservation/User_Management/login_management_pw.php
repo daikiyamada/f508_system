24aaaa03285939f47f77ea67dfb2dde75fc77777
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-10 16:10:48 +0900

    0210commit

--- Reservation/User_Management/login_management_pw.php
+++ Reservation/User_Management/login_management_pw.php
@@ -2,10 +2,11 @@
   require_once 'Manager.php';
   try{
     $db = connect();
-    $id = 'Manager';
-    $pw = 'Shinomilab';
-    if($_POST['pw']!=$pw){
-      if($_POST['ID']!=$id){
+    $id = "Manager";
+    $stt = $db->prepare('SELECT * FROM f508system WHERE ID=:ID');
+    $stt -> execute(array(':ID' => $id));
+    $usr = $stt -> fetch();
+    if(strcmp($usr['pw'],$_POST['pw'])==0 && strcmp($_POST['ID'],$id)==0){
       ?>
       <script type='text/javascript'>
       window.alert('管理アカウントのみログイン可能です。再度入力お願いします');
@@ -22,10 +23,9 @@
     ?>
     <script type='text/javascript'>
     window.alert('<?php print $usr['Name']?>さん、ログイン成功しました');
-    location.href = "system_menu.html";
+    location.href = "system_menu.php";
     </script>
     <?php
-    $db = NULL;
   }
   }catch (PDOException $e){
     exit("エラーが発生しました:{$e->getMessage()}");
