c7fef6d62981716e3c14e1a4d5a3b663cafeb1ae
yamadadaiki <daiki.baseball0516@gmail.com>
2020-02-17 18:26:05 +0900

    0217commit

--- Reservation/day_form.php
+++ Reservation/day_form.php
@@ -36,7 +36,6 @@ $day = substr($_GET["date"],6);
 <div class = "form">
 各コマの時間は、従来通りとなります。それに加えて、0コマは１コマ前の時間を示し、6コマは放課後を示しています。<br>
 </div>
-<div class="form2">
 <table>
   <tr><th>コマ</th><th>代表者名</th><th>使用目的</th><th>予約フォーム</th></tr>
   <?php
@@ -107,6 +106,5 @@ $day = substr($_GET["date"],6);
  }
   ?>
 </table>
-</div>
 </body>
 </html>
