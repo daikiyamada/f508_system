dfb0c0ff1a8f359748cb02512cf26e9ca1013a20
1558121 <e19m5210@soka-u.jp>
2020-03-16 13:24:47 +0900

    Fix exp.php

--- Reservation/exp.php
+++ Reservation/exp.php
@@ -22,7 +22,7 @@ try {
         $userData[] = $row;
         $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
         foreach($sth as $userData){
-            echo $jsonData["date"];
+            echo $jsonData;
         }
     }
 } catch (PDOException $e) {
