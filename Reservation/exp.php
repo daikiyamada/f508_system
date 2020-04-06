d3cf30406a8fbe8464ea028b97375fb6dfbeb939
1558121 <e19m5210@soka-u.jp>
2020-04-06 18:49:52 +0900

    Add SELECT date

--- Reservation/exp.php
+++ Reservation/exp.php
@@ -16,11 +16,11 @@ try {
     $sql = "SELECT date FROM Reservation Where date between $intd1 and $intd2";
     $sth = $dbh->prepare($sql);
     $sth->execute();
-    $userData = array();
+    // $userData = array();
 
     while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
         $userData[] = $row;
-        echo gettype($userData);
+        // echo gettype($userData);
         // $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
         // $jsdecode = json_decode($jsonData);
         // echo $row;
