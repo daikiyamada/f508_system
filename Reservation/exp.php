f880841fbe8df02b3ee09970cc21f856b14e6213
1558121 <e19m5210@soka-u.jp>
2020-04-11 23:20:39 +0900

    Fix exp.php

--- Reservation/exp.php
+++ Reservation/exp.php
@@ -36,7 +36,7 @@ try {
     // header('Content-type: application/json');
     $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
     // echo $jsonData;
-    echo $cnt[0];
+    echo $cnt[0][0];
     // echo '----';
 } catch (PDOException $e) {
     echo "接続失敗";
