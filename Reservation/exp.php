ed713f321a90d2a0244f59f4cd04560dae04c07a
1558121 <e19m5210@soka-u.jp>
2020-04-11 22:58:44 +0900

    Fix exp.php

--- Reservation/exp.php
+++ Reservation/exp.php
@@ -35,7 +35,7 @@ try {
     }
     // header('Content-type: application/json');
     $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
-    echo $substr($jsonData, 6);
+    // echo $substr($jsonData, 6);
     echo $jsonData;
     // echo $cnt;
     // echo '----';
