2483e1872397115b447ece210a5d145ec7659307
1558121 <e19m5210@soka-u.jp>
2020-04-12 13:30:52 +0900

    Fix exp.php

--- Reservation/exp.php
+++ Reservation/exp.php
@@ -37,8 +37,10 @@ try {
     $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
     // echo $jsonData;
     foreach($cnt as $key => $val){
-        echo 'key'."\n".$key;
-        echo 'date'."\n".$val['date'];
+        echo "key\n";
+        echo $key;
+        echo "date\n";
+        echo $val['date'];
         // echo 'date[]';
         // echo $val['date'][];
     }
