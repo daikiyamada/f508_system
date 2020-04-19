39e218c89e50aeb3e89d23e7457668e80d0bd607
1558121 <e19m5210@soka-u.jp>
2020-04-19 11:32:41 +0900

    Fix calendar.php

--- Reservation/calendar.php
+++ Reservation/calendar.php
@@ -3,31 +3,26 @@ require_once 'Manager.php';
 $Y = date("Y");
 $M = date("m");
 try {
-    $dbh = connect();
-    $date1 = $Y . $M . "01";
-    $date2 = $Y . $M . "31";
-    $int1 = intval($date1);
-    $int2 = intval($date2);
+  $dbh = connect();
+  $date1 = $Y . $M . "01";
+  $date2 = $Y . $M . "31";
+  $int1 = intval($date1);
+  $int2 = intval($date2);
 
-    $sql = "SELECT * FROM Reservation Where date between $int1 and $int2";
-    $sth = $dbh->prepare($sql);
-    $sth->execute();
-    for ($i = 1; $i < 32; $i++){
-        $cnt[$i]['c'] = 0;
-    }
-
-    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-        // $userData[] = $row;
-        // $M = substr($row['date'],4,2);
-        $date = (int)substr($row['date'],6,2);
-        $cnt[$date]['c'] += 1;
-    }
-    // $jsonData = json_encode($userData, JSON_UNESCAPED_UNICODE);
-    $CData = json_encode($cnt, JSON_UNESCAPED_UNICODE);
-    echo $CData;
+  $sql = "SELECT * FROM Reservation Where date between $int1 and $int2";
+  $sth = $dbh->prepare($sql);
+  $sth->execute();
+  for ($i = 1; $i < 32; $i++){
+    $cnt[$i]['c'] = 0;
+  }
+  while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+    $date = (int)substr($row['date'],6,2);
+    $cnt[$date]['c'] += 1;
+  }
+  $CData = json_encode($cnt, JSON_UNESCAPED_UNICODE);
 } catch (PDOException $e) {
-    echo "接続失敗";
-    echo $e->getMessage();
+  echo "接続失敗";
+  echo $e->getMessage();
 }
 $dbh = null;
 ?>
@@ -37,7 +32,7 @@ $dbh = null;
 <html xmlns="http://www.w3.org/1999/xhtml" xml="" lang="ja" lalng="ja" xml:lang="ja">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <head>
-    <title>F508予約システム</title>
+  <title>F508予約システム</title>
 </head>
 <link rel="stylesheet" type="text/css" href=" ../Homepage.css" />
 <body>
@@ -46,23 +41,21 @@ $dbh = null;
     <h1 id="title1">F508管理システム</h1>
   </div>
     <ul id="menu">
-        <li><a href="../index.html">Home</a></li>
-        <li><a href="User_Management/system_menu.html">ユーザ管理</a></li>
-        <li><a href="http://shinolab.tech">篠宮研究室</a></li>
-        <li><a href="http://teraylab.net/">寺島研究室</a></li>
-        <!-- <li><a href="exp.html">予約確認（仮）</a></li> -->
-        <li><a href="exp2.html">json確認</a></li>
-        <li><a href="form.html">予約確認（仮）</a></li>
+      <li><a href="../index.html">Home</a></li>
+      <li><a href="User_Management/system_menu.html">ユーザ管理</a></li>
+      <li><a href="http://shinolab.tech">篠宮研究室</a></li>
+      <li><a href="http://teraylab.net/">寺島研究室</a></li>
+      <li><a href="exp2.html">json確認</a></li>
+      <li><a href="form.html">予約確認（仮）</a></li>
     </ul>
     <div style="text-align: center;">
-    <button id="prev" type="button">前の月</button>
-    <button id="now" type="button">今月</button>
-    <button id="next" type="button">次の月</button>
-    <div id="calendar"></div>
+      <button id="prev" type="button">前の月</button>
+      <button id="now" type="button">今月</button>
+      <button id="next" type="button">次の月</button>
+      <div id="calendar"></div>
     <script type="text/javascript" src="calendar.js"></script>
     <script type="text/javascript">
       var cntData = JSON.parse('<?php echo $CData; ?>' || "null");
-      console.log(cntData);
     </script>
     </div>
     <script type="text/javascript" style="text-align: right;">
