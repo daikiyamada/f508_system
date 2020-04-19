906d6edaf67077489fbf86a5f0330635a12a8278
1558121 <e19m5210@soka-u.jp>
2020-04-19 21:53:15 +0900

    Fix calendar.php

--- Reservation/calendar.php
+++ Reservation/calendar.php
@@ -52,9 +52,115 @@ $dbh = null;
       <button id="now" type="button">今月</button>
       <button id="next" type="button">次の月</button>
       <div id="calendar"></div>
-    <script type="text/javascript" src="calendar.js"></script>
+    <!-- <script type="text/javascript" src="calendar.js"></script> -->
     <script type="text/javascript">
       var cntData = JSON.parse('<?php echo $CData; ?>' || "null");
+      const weeks = ['日', '月', '火', '水', '木', '金', '土']
+      const date = new Date()
+      let year = date.getFullYear() //年を取得
+      let month = date.getMonth() + 1 //月を取得（0～11）
+      let ty = date.getFullYear() // 今年を取得
+      let tm = date.getMonth() + 1 // 今月を取得
+      // ty tm を引数としてjson.phpにわたす
+      const config = {
+        show: 1,
+      }
+      /**カレンダー表示*/
+      function showCalendar(year, month) {
+        for (i = 0; i < config.show; i++) {
+          const calendarHtml = createCalendar(year, month)
+          const sec = document.createElement('section')
+          sec.innerHTML = calendarHtml
+          document.querySelector('#calendar').appendChild(sec)
+          month++
+          if (month > 12) {
+            year++
+            month = 1
+          }
+        }
+      }
+      function createCalendar(year, month) {
+        const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+        const endDate = new Date(year, month, 0) // 月の最後の日を取得
+        const endDayCount = endDate.getDate() // 月の末日
+        const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
+        const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+        const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+        let dayCount = 1 // 日にちのカウント
+        let calendarHtml = '' // HTMLを組み立てる変数
+        calendarHtml += '<h1>' + year + '/' + month + '</h1>'
+        calendarHtml += '<table>'
+        // 曜日の行を作成
+        for (let i = 0; i < weeks.length; i++) {
+          calendarHtml += '<td>' + weeks[i] + '</td>'
+        }
+        for (let w = 0; w < 6; w++) {
+          calendarHtml += '<tr>'
+          for (let d = 0; d < 7; d++) {
+            if (w == 0 && d < startDay) {
+              // 1行目で1日の曜日の前
+              let num = lastMonthendDayCount - startDay + d + 1
+              calendarHtml += '<td class="is-disabled">' + num + '</td>'
+            } else if (dayCount > endDayCount) {
+              // 末尾の日数を超えた
+              let num = dayCount - endDayCount
+              calendarHtml += '<td class="is-disabled">' + num + '</td>'
+              dayCount++
+            } else {
+              var yy = String(year)
+              if(month<10){
+                var mm = String('0'+month)
+              }
+              else{
+                var mm = String(month)
+              }
+              if(dayCount<10){
+                var dd = String('0'+dayCount)
+              }
+              else{
+                var dd = String(dayCount)
+              }
+              var date = yy+mm+dd
+              calendarHtml += `<td class="calendar_td" data-date=${date}>${dayCount}</td>`
+              dayCount++
+            }
+          }
+          calendarHtml += '</tr>'
+        }
+        calendarHtml += '</table>'
+        return calendarHtml
+      }
+      function moveCalendar(e) {
+        document.querySelector('#calendar').innerHTML = ''
+        if (e.target.id === 'prev') {
+          month--
+          if (month < 1) {
+            year--
+            month = 12
+          }
+        }
+        if (e.target.id === 'now') {
+          year = ty
+          month = tm
+        }
+        if (e.target.id === 'next') {
+          month++
+          if (month > 12) {
+            year++
+            month = 1
+          }
+        }
+        showCalendar(year, month)
+      }
+      document.querySelector('#prev').addEventListener('click', moveCalendar)
+      document.querySelector('#now').addEventListener('click', moveCalendar)
+      document.querySelector('#next').addEventListener('click', moveCalendar)
+      document.addEventListener("click", function (e) {
+      if (e.target.classList.contains("calendar_td")) {
+        location.href="day_form.php?date="+e.target.dataset.date;
+      }
+    })
+    showCalendar(year, month)
     </script>
     </div>
     <script type="text/javascript" style="text-align: right;">
