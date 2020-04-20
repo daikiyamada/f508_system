6b0dc607008b0273a23cad83acceb657730e3984
1558121 <e19m5210@soka-u.jp>
2020-04-20 18:04:46 +0900

    Fix calendar.php

--- Reservation/calendar.php
+++ Reservation/calendar.php
@@ -79,12 +79,13 @@ $dbh = null;
           }
         }
       }
+      // カレンダー作成
       function createCalendar(year, month) {
         const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
         const endDate = new Date(year, month, 0) // 月の最後の日を取得
         const endDayCount = endDate.getDate() // 月の末日
-        const lastMonthEndDate = new Date(year, month - 2, 0) // 前月の最後の日の情報
-        const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+        const lastMonthEndDate = new Date(year, month - 1, 0) // 前月の最後の日の情報
+        const lastMonthEndDayCount = lastMonthEndDate.getDate() // 前月の末日
         const startDay = startDate.getDay() // 月の最初の日の曜日を取得
         let dayCount = 1 // 日にちのカウント
         let calendarHtml = '' // HTMLを組み立てる変数
@@ -93,13 +94,14 @@ $dbh = null;
         // 曜日の行を作成
         for (let i = 0; i < weeks.length; i++) {
           calendarHtml += '<td>' + weeks[i] + '</td>'
-        }
+        } //曜日を表示
+        //日付を作成
         for (let w = 0; w < 6; w++) {
           calendarHtml += '<tr>'
           for (let d = 0; d < 7; d++) {
             if (w == 0 && d < startDay) {
               // 1行目で1日の曜日の前
-              let num = lastMonthendDayCount - startDay + d + 1
+              let num = lastMonthEndDayCount - startDay + d + 1
               calendarHtml += '<td class="is-disabled">' + num + '</td>'
             } else if (dayCount > endDayCount) {
               // 末尾の日数を超えた
@@ -130,6 +132,7 @@ $dbh = null;
         calendarHtml += '</table>'
         return calendarHtml
       }
+      // 月移動
       function moveCalendar(e) {
         document.querySelector('#calendar').innerHTML = ''
         if (e.target.id === 'prev') {
